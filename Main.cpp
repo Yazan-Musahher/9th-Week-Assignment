2dac5a72d97ae932e3ecd14050e8cd92018b5f8a
Mohamad Yazan Musahher <Mohamadym@uia.no>
2022-10-18 11:52:50 +0200

    last

--- Main.cpp
+++ Main.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
 
 
-struct Node{
-    int data;
-    Node* left;
-    Node* right;
+struct node
+{
+    char name[30];
+    int number[15];
+    char address[40];
+    char email[40];
+    struct node *left, *right;
 };
-//how to creat a new Node
-Node* createNewNode(int data) {
-    Node* newNode = new Node();
-    newNode->data = data;
-    newNode->left = newNode->right = nullptr;
-    return newNode;
+
+
 
 }
 
 int main () {
-
-    Node* root = createNewNode(1);
-
-    root->left = createNewNode(2);
-
-    root->right = createNewNode(3);
-
-    //new one
-
-    root->left->left = createNewNode(4);
-    root->left->right = createNewNode(5);
-    root->right->left = createNewNode(5);
-
-
 
 
 
