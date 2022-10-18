0f8ca60175780760d5b7eb61ba92602d106559ec
Mohamad Yazan Musahher <mohamed.musahher@gmail.com>
2022-10-18 07:42:39 +0200

    updated

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
 
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+//how to creat a new Node
+Node* createNewNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+
+}
+
 int main () {
+
+
+
 
 
     return 0;
